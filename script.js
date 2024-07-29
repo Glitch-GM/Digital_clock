bdf749e8a80b7852e657741e046cb252fe072121
Glitch <gulshanmurmu23@gmail.com>
2024-07-29 11:10:16 +0530

    time interval after 1s

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const clock = document.getElementById("clock");
 
-let date = new Date();
-console.log(date.toLocaleTimeString());
+setInterval(function () {
+  let date = new Date();
+  console.log(date.toLocaleTimeString());
+}, 1000);
